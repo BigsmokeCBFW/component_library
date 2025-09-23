0b888a1c0c8feb8291cd82925865b6a7e631c97b
Ahmed Omeiza <omeizaahmed81@gmail.com>
2025-09-23 10:20:40 +0100

    second commit

--- badges/src/App.jsx
+++ badges/src/App.jsx
@@ -1,4 +1,14 @@
+import Badge from "./components/Badge.jsx"
+
 export default function App(){
   
-  return
+  return(
+    <>
+      <Badge edge="pill" color="indigo">This was a badge created by Say Gang!</Badge>
+      <Badge>Big Blrrd</Badge>
+      <Badge edge="pill">Big Shiest</Badge>
+      <Badge edge="square" color="red"> Big Slime</Badge>
+      <Badge edge="pill">Big Wham</Badge>
+    </>
+  )
 }
--- badges/src/components/Badge.jsx
+++ badges/src/components/Badge.jsx
@@ -1,4 +1,19 @@
-export default function Badge ({children, edge}){
 
-  return
+function randColor(){
+  const colorArr = ['gray', 'red', 'yellow', 'blue', 'green', 'indigo', 'purple', 'pink']
+  const randIndex = Math.floor(Math.random() * colorArr.length)
+
+  return colorArr[randIndex]
+}
+
+export default function Badge ({children, edge="square", color=''}){
+
+  color = color? color: randColor()
+  const styleClass = `${edge} ${color}`
+
+  return (
+    <div className={styleClass}>
+      {children}
+    </div>
+    )
 }
